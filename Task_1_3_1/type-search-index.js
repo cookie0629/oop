bbe6cc046ad573a72c2a6f9be13dd90eac53dd06
cookie0629 <cookie0629@users.noreply.github.com>
2024-11-19 13:57:51 +0000

    Deploying to gh-pages from @ cookie0629/oop@b19d7bc19a34af4fa275b60cb247d5aec4797f7a 🚀

--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.zhao","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.zhao","l":"SubstringFinder"}]
