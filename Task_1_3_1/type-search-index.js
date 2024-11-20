f08fdde77fe530c0558afd995af5214e67e38835
cookie0629 <cookie0629@users.noreply.github.com>
2024-11-20 05:30:46 +0000

    Deploying to gh-pages from @ cookie0629/oop@56f69b8f61198c1200ed8498072cb87ed4590e29 🚀

--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.zhao","l":"SubstringSearch"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.zhao","l":"ResourceReadException"},{"p":"ru.nsu.zhao","l":"SubstringSearch"}]
