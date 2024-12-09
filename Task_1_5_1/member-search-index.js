e481e613809e484bc2c5de91eac739e852005bc9
cookie0629 <cookie0629@users.noreply.github.com>
2024-12-09 15:11:41 +0000

    Deploying to gh-pages from @ cookie0629/oop@17999b3d7631c83b302296cc110c5862d8fd4c63 🚀

---
+++ Task_1_5_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.zhao","c":"MarkdownGenerator","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"ru.nsu.zhao","c":"MarkdownGenerator","l":"MarkdownGenerator()","url":"%3Cinit%3E()"}]
