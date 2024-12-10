ffea8b23190d3b91ae81661d8bd4e1790badf1f9
Songtao Zhao <s.chzhao4@g.nsu.ru>
2024-12-10 09:57:57 +0700

    tijiao

--- Task_1_5_1/src/test/java/ru/nsu/zhao/BlockquoteTest.java
+++ Task_1_5_1/src/test/java/ru/nsu/zhao/BlockquoteTest.java
@@ -1,44 +1,53 @@
 package ru.nsu.zhao;
 
 import org.junit.jupiter.api.Test;
-import ru.nsu.zhao.markdown.Text;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TextTests {
+/**
+ * 测试 Blockquote 类。
+ */
+class BlockquoteTest {
 
     @Test
-    void testBoldSerialization() {
-        Text.Bold boldText = new Text.Bold("bold");
-        assertEquals("**bold**", boldText.toMarkdown());
+    void testToMarkdownSingleLine() {
+        Blockquote blockquote = new Blockquote("This is a blockquote.");
+        String expectedMarkdown = "> This is a blockquote.";
+        assertEquals(expectedMarkdown, blockquote.toMarkdown(), "单行引用的 Markdown 序列化失败");
     }
 
     @Test
-    void testItalicSerialization() {
-        Text.Italic italicText = new Text.Italic("italic");
-        assertEquals("*italic*", italicText.toMarkdown());
+    void testToMarkdownMultiLine() {
+        Blockquote blockquote = new Blockquote("This is a blockquote.\nIt has multiple lines.");
+        String expectedMarkdown = "> This is a blockquote.\n> It has multiple lines.";
+        assertEquals(expectedMarkdown, blockquote.toMarkdown(), "多行引用的 Markdown 序列化失败");
     }
 
     @Test
-    void testStrikethroughSerialization() {
-        Text.Strikethrough strikethroughText = new Text.Strikethrough("strikethrough");
-        assertEquals("~~strikethrough~~", strikethroughText.toMarkdown());
+    void testEqualsSameContent() {
+        Blockquote blockquote1 = new Blockquote("Same content");
+        Blockquote blockquote2 = new Blockquote("Same content");
+        assertEquals(blockquote1, blockquote2, "具有相同内容的 Blockquote 对象不相等");
     }
 
     @Test
-    void testInlineCodeSerialization() {
-        Text.InlineCode inlineCode = new Text.InlineCode("code");
-        assertEquals("`code`", inlineCode.toMarkdown());
+    void testNotEqualsDifferentContent() {
+        Blockquote blockquote1 = new Blockquote("Content A");
+        Blockquote blockquote2 = new Blockquote("Content B");
+        assertNotEquals(blockquote1, blockquote2, "具有不同内容的 Blockquote 对象相等");
     }
 
     @Test
-    void testTextEquality() {
-        Text.Bold text1 = new Text.Bold("bold");
-        Text.Bold text2 = new Text.Bold("bold");
-        assertEquals(text1, text2);
-        assertEquals(text1.hashCode(), text2.hashCode());
+    void testHashCodeSameContent() {
+        Blockquote blockquote1 = new Blockquote("Same content");
+        Blockquote blockquote2 = new Blockquote("Same content");
+        assertEquals(blockquote1.hashCode(), blockquote2.hashCode(), "相同内容的 Blockquote 对象的哈希值不同");
+    }
 
-        Text.Bold differentText = new Text.Bold("different");
-        assertNotEquals(text1, differentText);
+    @Test
+    void testHashCodeDifferentContent() {
+        Blockquote blockquote1 = new Blockquote("Content A");
+        Blockquote blockquote2 = new Blockquote("Content B");
+        assertNotEquals(blockquote1.hashCode(), blockquote2.hashCode(), "不同内容的 Blockquote 对象的哈希值相同");
     }
 }
