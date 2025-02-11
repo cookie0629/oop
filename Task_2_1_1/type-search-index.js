cf151141dec36606197bf3ccc985f19d7298f9ac
cookie0629 <cookie0629@users.noreply.github.com>
2025-02-11 07:52:16 +0000

    Deploying to gh-pages from @ cookie0629/oop@542fae5597aeb5e2a1354efeaa663ba2ab39fa87 🚀

--- Task_2_1_1/type-search-index.js
+++ Task_2_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.zhao","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.zhao","l":"PrimeCheck"}];updateSearchResults();
